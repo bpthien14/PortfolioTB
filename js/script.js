1cb50183a1e195c6cc94a9f61118ec7b2822fcc2
bpthien14 <bpthien21@clc.fitus.edu.vn>
2024-02-22 16:24:30 +0700

    add reponsive header

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
 
 //scroll sections
 
@@ -27,5 +35,9 @@ window.onscroll = () => {
   let header = document.querySelector('header');
 
   header.classList.toggle('sticky', window.scrollY > 100);
+
+  // remove toggle icon and navbar when click navbar links
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active');
 }
 
